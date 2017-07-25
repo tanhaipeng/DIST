ddfa7d0fd8fc0b9f00402ccaa2cae36bb8f1a881
tanhaipeng <tanhaipeng@baidu.com>
2017-07-25 16:18:45 +0800

    init modules

---
+++ control/dist_control.go
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2017 DIST Author. All Rights Reserved.
+ * DIST Control Module Drivered by Golang
+ * 2017/7/25, by Tan Haipeng, create
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("DIST Control Module")
+}
